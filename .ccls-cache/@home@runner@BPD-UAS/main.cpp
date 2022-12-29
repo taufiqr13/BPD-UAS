81fdfaba8938167b7f65a9c06828cd547a088927
Taufiq Rahmanda <56916214+taufiqr13@users.noreply.github.com>
2022-12-29 14:59:13 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@BPD-UAS/main.cpp
@@ -0,0 +1,21 @@
+int main() { 
+ int i; 
+ float nilai[10];  
+  
+  
+         printf("Masukkan nilai mahasiswa:\n"); 
+                 for (i = 0; i < 10; i++) { 
+                 printf("Input nilai mahasiswa ke-%d: ", i+1); 
+                 scanf("%f", &nilai[i]); 
+                 } 
+  
+  
+         printf("\nNilai mahasiswa yang lulus:\n"); 
+                 for (i = 0; i < 10; i++) { 
+                         if (nilai[i] >= 60) { 
+                         printf("Nilai mahasiswa ke-%d: %.2f\n", i+1, nilai[i]); 
+                 }         
+         } 
+  
+         return 0; 
+  }
